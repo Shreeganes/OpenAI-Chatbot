06a253360024342227fd194237886bfff44683dc
Shreeganesh A <126757494+Shreeganes@users.noreply.github.com>
2024-12-03 20:13:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.llms import Ollama
 import os
 
 import os
@@ -13,7 +12,7 @@ load_dotenv()
 ## Langsmith Tracking
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With Ollama"
+os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With OPENAI"
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
@@ -23,8 +22,10 @@ prompt=ChatPromptTemplate.from_messages(
     ]
 )
 
-def generate_response(question,llm,temperature,max_tokens):
-    llm=Ollama(model=llm)
+def generate_response(question,api_key,engine,temperature,max_tokens):
+    openai.api_key=api_key
+
+    llm=ChatOpenAI(model=engine)
     output_parser=StrOutputParser()
     chain=prompt|llm|output_parser
     answer=chain.invoke({'question':question})
@@ -34,8 +35,13 @@ def generate_response(question,llm,temperature,max_tokens):
 st.title("Enhanced Q&A Chatbot With OpenAI")
 
 
+
+## Sidebar for settings
+st.sidebar.title("Settings")
+api_key=st.sidebar.text_input("Enter your Open AI API Key:",type="password")
+
 ## Select the OpenAI model
-llm=st.sidebar.selectbox("Select Open Source model",["mistral"])
+engine=st.sidebar.selectbox("Select Open AI model",["gpt-4o","gpt-4-turbo","gpt-4"])
 
 ## Adjust response parameter
 temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
@@ -45,11 +51,12 @@ max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=
 st.write("Goe ahead and ask any question")
 user_input=st.text_input("You:")
 
-
-
-if user_input :
-    response=generate_response(user_input,llm,temperature,max_tokens)
+if user_input and api_key:
+    response=generate_response(user_input,api_key,engine,temperature,max_tokens)
     st.write(response)
+
+elif user_input:
+    st.warning("Please enter the OPen AI aPi Key in the sider bar")
 else:
     st.write("Please provide the user input")
 
